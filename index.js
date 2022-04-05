49b69bb28852997c40c88aaece69a3d45e78b431
Kirsten <kirsten.staggs@gmail.com>
2022-04-04 19:52:27 -0600

    commit tree 2ca8d18259ce6c486ae5cff4b4dc857dfc6f98c9

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
 
+
+const random = Math.floor(Math.random() + 1);
+
+const num3 = 4;
+const num4 = 1;
+const mod = num3 / num4;
+
+const max = 20;
 // Write your code here
